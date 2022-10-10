1ec78de3fd06749a9015c89d0227fa726963cb07
Ashwini <ashwinikanawade91@gmail.com>
2022-10-10 17:18:33 +0530

    My First Commit

---
+++ src/main/java/in/ashokit/binding/User.java
@@ -0,0 +1,39 @@
+package in.ashokit.binding;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String uname;
+	private String pwd;
+	private String email;
+	private Long phno;
+	
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getPhno() {
+		return phno;
+	}
+	public void setPhno(Long phno) {
+		this.phno = phno;
+	}
+	
+	
+}
